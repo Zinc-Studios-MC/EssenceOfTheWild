a5641515ea4c657221d723032f3783be28b95602
Daragetsu <nabbonoushad@gmail.com>
2026-06-22 18:11:09 +0600

    burrow textures

--- src/main/java/net/mrmisc/essenceofthewild/block/custom/burrow/BurrowBlock.java
+++ src/main/java/net/mrmisc/essenceofthewild/block/custom/burrow/BurrowBlock.java
@@ -5,6 +5,7 @@ import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.LevelAccessor;
 import net.minecraft.world.level.block.BaseEntityBlock;
+import net.minecraft.world.level.block.RenderShape;
 import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.entity.BlockEntityTicker;
 import net.minecraft.world.level.block.entity.BlockEntityType;
@@ -38,5 +39,9 @@ public class BurrowBlock extends BaseEntityBlock{
             bbe.extractFerrets();
         }
         return super.onDestroyedByPlayer(state, level, pos, player, willHarvest, fluid);
+    }
+    @Override
+    public RenderShape getRenderShape(BlockState pState) {
+        return RenderShape.MODEL;
     }
 }
